8352837d11c46c6010e0bb10684afd31136b8da4
Kiselev Aleksandr <88896936+Can4e3@users.noreply.github.com>
2021-08-13 22:31:58 +0300

    Add files via upload

---
+++ SqureFigures/Squares.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareFigures
+{
+    public static class Squares
+    {
+        public static double GetSquare(double A, double B, double C)
+        {
+            double s;
+            double p;
+            p = (A + B + C) / 2;
+            s = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return s;
+        }
+        public static double GetSquare(double R)
+        {
+            double s;
+            s = 2*Math.PI*R*R;
+            return s;
+        }
+    }
+}
